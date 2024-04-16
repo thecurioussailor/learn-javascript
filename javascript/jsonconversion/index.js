f442e662b5891e28ad6345a7001616773e54e61e
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-04-16 16:28:15 +0530

    jsonconversion

---
+++ javascript/jsonconversion/index.js
@@ -0,0 +1,18 @@
+
+// JSON - JavaScript Object Notation
+
+// const user = {
+//     'firstName': "Aishwarya", // keys can be written in qoutes "" or ''
+//     'age': 40,
+//     'gender': "male"
+// }
+const user = {
+    firstName: "Aishwarya",
+    age: 40,
+    gender: "male"
+}
+
+const data = JSON.stringify(user); // it converts your json data into string
+
+const data2 = JSON.parse(data); // it converts your string into json data
+console.log(data2);
