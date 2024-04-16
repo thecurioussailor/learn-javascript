099fd4cb6e8d4207564d4833dda3e8aba72967ba
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-04-16 16:21:03 +0530

    added content

--- javascript/filereading/index.js
+++ javascript/filereading/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+// fs i.e. file system is in nodejs library
 fs.readFile("a.txt", "utf-8", function(err, data) {
     console.log(data);
 })
--- javascript/synchronousVsAsyncrnous/index.js
+++ javascript/synchronousVsAsyncrnous/index.js
@@ -5,22 +5,34 @@
 // Asynchronous is a non-blocking architecture, so the execution of one task isn't dependent on another. Tasks can run simultaneously. 
 // Synchronous is a blocking architecture, so the execution of each operation depends on completing the one before it. 
 // Each task requires an answer before moving on to the next iteration.
-
+const fs = require('fs');
 function greet(){
     console.log("Hello");
 }
 function looping() {
     let a = 0;
-for(let i = 0; i < 1000000000; i++){
+    for(let i = 0; i < 1000000000; i++){
 
-    a = a + 2;
+        a = a + 2;
+    }
+    console.log(a);
 }
-console.log(a);
-}
+
 
 console.log("first");
+fs.readFile("../filereading/a.txt", "utf-8", function (err, data) {
+    console.log(data);
+})
 
+setTimeout(function () {
+    console.log("I am setTime out")
+},100);
 console.log("second");
-looping();
+looping(); // synchronous function
 greet();
 console.log("third");
+
+// fs function is an asynchronous function
+
+
+
