7b00d5fa8c0880f510ec5118d8917d7b29f3d835
Ashutosh Sagar <48312681+thecurioussailor@users.noreply.github.com>
2024-04-22 20:00:59 +0530

    promises

---
+++ javascript/promises/index.js
@@ -0,0 +1,103 @@
+// // Callback hell
+// //Multiple callback functions would create callback hell that leads to unmanageable code.
+
+// // function square(a, callback){                                           // square(3, multiplyBy2)
+// //     const value = callback(a);                                          // const value = 6
+// //     return value*value;                                                    // return 6*6
+// // }
+
+// // function multiplyBy2(a){                                                // multiplyBy2(3)
+// //     return 2*a;                                                         // return 2 * 3
+// // }
+
+// // function ssum(a,b, callback){                                                // ssum (2,3,square)
+// //     return callback(a,multiplyBy2) + callback(b, multiplyBy2);              // return 16 + 36
+// // }
+
+// // console.log(ssum(2,3,square));
+
+// //Promises
+// //Promises are used to handle asynchronous operations in JavaScript.
+
+// // setTimeout(() =>{
+// //     console.log("hiiii")
+// // }, 3000);
+
+// //
+
+// // function sum1(a , b , cb) {
+// //     return cb(a + b);
+// // }
+
+// // function onDone(data){
+// //     console.log(data);
+// // }
+
+// // sum1(2,3,onDone);
+
+// // function sum(a,b){
+// //     let promise = new Promise(function (resolve,reject){
+// //     let sum = a + b;
+
+// //         resolve(sum);
+// // })
+// //   return promise;
+// // }
+
+// // function onDone(data){
+// //     console.log(data);
+// // //     return new error;
+// // }
+
+// // sum(2,3)
+// //     .then(onDone)
+// //                 .catch((error)=>{
+// //                     console.log("something is wrong")
+// //                 })
+
+// function square(a){                                           // square(3, multiplyBy2)                                          // const value = 6
+//         return new Promise(function(resolve, reject){
+//             let square1 = a*a;
+//             resolve(square1)
+//         })                                               // return 6*6
+//     }
+    
+//     function multiplyBy2(a){    
+//                                                     // multiplyBy2(3)
+//         return new Promise(function (resolve, reject) {
+//             resolve(2*a)
+//         })                                                         // return 2 * 3
+//     }
+
+//     function onDone(data){
+//         console.log(data);
+//     }
+    
+//     square(3).then(multiplyBy2).then(onDone);
+
+function square(a){
+    return new Promise((resolve) => {
+        resolve(a * a);
+    });
+}
+
+function multiplyBy2(a){
+    return new Promise((resolve) => {
+        resolve(2 * a);
+    });
+}
+
+function ssum(a, b){
+    return Promise.all([multiplyBy2(a), multiplyBy2(b)])
+        .then(([valueA, valueB]) => {
+            return valueA + valueB;
+        });
+}
+
+// Example usage:
+ssum(2, 3).then();
+
+    
+ 
+
+    
